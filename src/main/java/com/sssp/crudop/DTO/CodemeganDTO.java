6019d743c315c00f2b02ec24261c89c82aad1844
Suryaprakash-Shankar <surya055cs@gmail.com>
2025-06-25 18:11:55 +0530

    without an DTO

--- src/main/java/com/sssp/crudop/DTO/CodemeganDTO.java
+++ src/main/java/com/sssp/crudop/DTO/CodemeganDTO.java
@@ -1,7 +1,19 @@
 package com.sssp.crudop.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.security.PrivateKey;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CodemeganDTO {
 
+    private String Name;
+    private String Age;
+    private String Address;
 
 
 }
---
+++ src/main/java/com/sssp/crudop/Services/CodemeganDTOService.java
@@ -0,0 +1,17 @@
+//package com.sssp.crudop.Services;
+//
+//import com.sssp.crudop.repository.CodemeganRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//
+//public class CodemeganDTOService {
+//
+//    @Autowired
+//
+//    private CodemeganRepo codemeganRepo;
+//
+//
+//
+//}
